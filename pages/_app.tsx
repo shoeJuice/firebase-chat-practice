ba4d7f3cf41b025e140ad574ac3c13b13571eb01
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-08-06 01:50:29 -0400

    Changes: - Initialize firebase, store data in environment variables - Set up authentication context and logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { AuthenticationProvider } from "../context/AuthenticationContext";
+import {useRouter} from 'next/router'
+import ProtectedRoute from "../modules/auth/ProtectedRoute";
+import Navbar from "../modules/nav/Navbar";
+
+const allowedRoutes = ["/", "/login", "/register"];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+
+
+  return (
+    <AuthenticationProvider>
+      <Navbar />
+      {allowedRoutes.includes(router.pathname) ? <Component {...pageProps} /> : <ProtectedRoute><Component {...pageProps} /></ProtectedRoute>}
+    </AuthenticationProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
