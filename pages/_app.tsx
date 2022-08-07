0cddc814105d81baf561c251e0884d62a4e191be
tinyFireFly <remsfield.papillon@student.fairfield.edu>
2022-08-06 20:26:55 -0400

    Changes: - Set up input to send messages and userInfo - Set up client to add userinfo to rooms subcollection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,25 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthenticationProvider } from "../context/AuthenticationContext";
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 import ProtectedRoute from "../modules/auth/ProtectedRoute";
 import Navbar from "../modules/nav/Navbar";
 
 const allowedRoutes = ["/", "/login", "/register"];
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   const router = useRouter();
-
 
   return (
     <AuthenticationProvider>
       <Navbar />
-      {allowedRoutes.includes(router.pathname) ? <Component {...pageProps} /> : <ProtectedRoute><Component {...pageProps} /></ProtectedRoute>}
+      {allowedRoutes.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <ProtectedRoute>
+          <Component {...pageProps} />
+        </ProtectedRoute>
+      )}
     </AuthenticationProvider>
   );
 }
